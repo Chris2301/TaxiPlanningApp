01c7a00bfb436b227f7525b0ab9c43654fd5e090
Chris2301 <34375616+Chris2301@users.noreply.github.com>
2018-02-21 15:15:43 +0100

    tweede poging

--- src/main/java/nl/qien/taxi/TaxiApplication.java
+++ src/main/java/nl/qien/taxi/TaxiApplication.java
@@ -2,11 +2,26 @@ package nl.qien.taxi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import nl.qien.taxi.domain.MailMail;
+import nl.qien.taxi.domain.Taxi;
 
 @SpringBootApplication
 public class TaxiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaxiApplication.class, args);
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Mail.xml");
+
+	    	MailMail mm = (MailMail) context.getBean("mailMail");
+	 
+	    	mm.sendMail("TaxiApp1234@gmail.com",
+	    		   "TaxiApp1234@gmail.com",
+	    		   "Hello Guy",
+	    		   "PoogelyWoopski \n\n Dit werkt al in ieder geval");
+	    	
 	}
 }
